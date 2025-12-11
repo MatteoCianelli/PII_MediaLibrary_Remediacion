92bf80b0f939ba46fdbd7b670e2e4a2119f30b0b
Matteo <MATTEO.CIANELLI@correo.ucu.edu.uy>
2025-12-11 11:12:27 -0300

    Parte 3 sin terminar

--- src/Library/AudioBookRepository.cs
+++ src/Library/AudioBookRepository.cs
@@ -1,7 +1,48 @@
+using System.Collections.Generic;
+
 namespace Ucu.Poo.Library
 {
     public class AudioBookRepository
     {
+        private List<AudioBook> audioBooks = new List<AudioBook>();
+
+        public IReadOnlyCollection<AudioBook> Items
+        {
+            get
+            {
+                return this.audioBooks.AsReadOnly();
+            }
+        }
         
+        public AudioBook FindById(string id)
+        {
+            foreach (AudioBook audioBook in audioBooks)
+            {
+                if (audioBook.CheckId(id))
+                {
+                    return audioBook;
+                }
+            }
+            
+            return null;
+        }
+        
+        public AudioBook FindByTitle(string title)
+        {
+            foreach (AudioBook audioBook in audioBooks)
+            {
+                if (audioBook.Title == title)
+                {
+                    return audioBook;
+                }
+            }
+            
+            return null;
+        }
+
+        public void Add(AudioBook audioBook)
+        {
+            this.audioBooks.Add(audioBook);
+        }
     }
 }
